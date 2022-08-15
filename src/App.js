e62005a368681895d2bf3f0d0d143e81d11293f0
Siddhartha <siddharthasahu.work@gmail.com>
2022-08-16 01:32:25 +0530

    feat: code

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {Cart, Home, Product, ProductPage, UserProfile, Wishlist, Checkout, Error} from "./pages"
 import Mockman from "mockman-js";
 import Navbar from "./component/Navbar/Navbar"
+import {Login, Signup} from "./pages/Auth"
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello</h1>
+      <Navbar />
     </div>
   );
 }
